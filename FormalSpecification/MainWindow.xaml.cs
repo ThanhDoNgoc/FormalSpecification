9e42da7aaf42ef8f6022166fae74536222838063
unknown <18520358@gm.uit.edu.vn>
2021-06-15 22:25:36 +0700

    initial commit

---
+++ FormalSpecification/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+using ScintillaNET;
+using ScintillaNET.WPF;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+
+namespace FormalSpecification
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbOutput.WrapMode = tbInput.WrapMode = WrapMode.None;
+            tbOutput.IndentationGuides = tbInput.IndentationGuides = IndentView.LookBoth;
+
+
+            InitColors(tbOutput);
+            InitColors(tbInput);
+            InitSyntaxColoring(tbOutput);
+            InitSyntaxColoring(tbInput);
+            InitNumberMargin(tbOutput);
+            InitNumberMargin(tbInput);
+            tbInput.Styles[ScintillaNET.Style.Cpp.Word2].ForeColor = System.Drawing.Color.Blue;
+            tbInput.SetKeywords(1, "R Z N def void Null ArgumentError arguments Array Boolean Class Date DefinitionError Error EvalError Function int Math Namespace Number Object RangeError ReferenceError RegExp SecurityError String SyntaxError TypeError uint XML XMLList Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 IntPtr SByte Single UInt16 UInt32 UInt64 UIntPtr Void Path System File Windows Forms ScintillaNET");
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            tbInput.Text = "";
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "TXT|*.txt";
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                var str = File.ReadAllText(filePath);
+                tbInput.Text = str;
+            }
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt | All Files (*.*)|*.*";
+
+
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(File.Create(saveFileDialog.FileName));
+                sw.Write(tbInput.Text);
+                sw.Dispose();
+            }           
+        }
+        public static System.Drawing.Color IntToColor(int rgb)
+        {
+            return System.Drawing.Color.FromArgb(255, (byte)(rgb >> 16), (byte)(rgb >> 8), (byte)rgb);
+        }
+        private void InitNumberMargin(ScintillaNET.WPF.ScintillaWPF TextArea)
+        {
+            //TextArea.Styles[ScintillaNET.Style.LineNumber].BackColor = IntToColor(0x212121);
+            TextArea.Styles[ScintillaNET.Style.LineNumber].ForeColor = System.Drawing.Color.DarkBlue;
+            TextArea.Styles[ScintillaNET.Style.IndentGuide].ForeColor = IntToColor(0xB7B7B7);
+            TextArea.Styles[ScintillaNET.Style.IndentGuide].BackColor = IntToColor(0x2A211C);
+
+            var nums = TextArea.Margins[0];
+            nums.Width = 30;
+            nums.Type = MarginType.Number;
+            nums.Sensitive = true;
+            nums.Mask = 0;
+
+
+        }
+        private void InitSyntaxColoring(ScintillaNET.WPF.ScintillaWPF TextArea)
+        {
+
+            // Configure the default style
+            TextArea.StyleResetDefault();
+            TextArea.Styles[ScintillaNET.Style.Default].Font = "Consolas";
+            TextArea.Styles[ScintillaNET.Style.Default].Size = 10;
+
+            TextArea.StyleClearAll();
+
+            // Configure the CPP (C#) lexer styles
+            TextArea.Styles[ScintillaNET.Style.Cpp.Identifier].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.Comment].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.CommentLine].ForeColor = System.Drawing.Color.DarkGreen;
+            TextArea.Styles[ScintillaNET.Style.Cpp.CommentDoc].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.Number].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.String].ForeColor = System.Drawing.Color.DarkRed;
+            TextArea.Styles[ScintillaNET.Style.Cpp.Character].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.Preprocessor].ForeColor = IntToColor(0x8AAFEE);
+            TextArea.Styles[ScintillaNET.Style.Cpp.Operator].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.Regex].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.CommentLineDoc].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.Word].ForeColor = System.Drawing.Color.Blue;
+            TextArea.Styles[ScintillaNET.Style.Cpp.Word2].ForeColor = System.Drawing.Color.Blue;
+            TextArea.Styles[ScintillaNET.Style.Cpp.CommentDocKeyword].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.CommentDocKeywordError].ForeColor = System.Drawing.Color.Black;
+            TextArea.Styles[ScintillaNET.Style.Cpp.GlobalClass].ForeColor = System.Drawing.Color.Black;
+
+            TextArea.Lexer = Lexer.Cpp;
+
+            TextArea.SetKeywords(0, "pre post class extends implements import interface new case do while else if for in switch throw get set function var try catch finally while with default break continue delete return each const namespace package include use is as instanceof typeof author copy default deprecated eventType example exampleText exception haxe inheritDoc internal link mtasc mxmlc param private return see serial serialData serialField since throws usage version langversion playerversion productversion dynamic private public partial static intrinsic internal native override protected AS3 final super this arguments null Infinity NaN undefined true false abstract as base bool break by byte case catch char checked class const continue decimal default delegate do double descending explicit event extern else enum false finally fixed float for foreach from goto group if implicit in int interface internal into is lock long new null namespace object operator out override orderby params private protected public readonly ref return switch struct sbyte sealed short sizeof stackalloc static string select this throw true try typeof uint ulong unchecked unsafe ushort using var virtual volatile void while where yield");
+            TextArea.SetKeywords(1, "def void Null ArgumentError arguments Array Boolean Class Date DefinitionError Error EvalError Function int Math Namespace Number Object RangeError ReferenceError RegExp SecurityError String SyntaxError TypeError uint XML XMLList Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 IntPtr SByte Single UInt16 UInt32 UInt64 UIntPtr Void Path System File Windows Forms ScintillaNET");
+
+        }
+
+        private void InitColors(ScintillaNET.WPF.ScintillaWPF TextArea)
+        {
+            TextArea.CaretForeColor = Colors.White;
+            TextArea.SetSelectionBackColor(true, IntToMediaColor(0x114D9C));
+
+        }
+        public static System.Windows.Media.Color IntToMediaColor(int rgb)
+        {
+            return System.Windows.Media.Color.FromArgb(255, (byte)(rgb >> 16), (byte)(rgb >> 8), (byte)rgb);
+        }
+
+        private void btnCplusplus_Click(object sender, RoutedEventArgs e)
+        {
+            string str = tbInput.Text;
+            str = Regex.Replace(str, @"\s+",string.Empty);
+            tbOutput.Text = str;
+            Match pre = Regex.Match(str, "pre");
+            Match post = Regex.Match(str, "post");
+
+            if (pre.Success && post.Success )
+            {
+                tbOutput.Text = str ;
+                Provider provider = new Provider(str, tbInput.Text);
+            }
+        }
+
+        private void btnCsharp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FormalSpecification/Provider.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace FormalSpecification
+{
+    class Provider
+    {
+
+        string function = "";
+        string pre = "";
+        string post = "";
+
+        string function_name = "";
+        Dictionary<string, string> function_variable = new Dictionary<string, string>();
+        KeyValuePair<string, string> function_result;
+
+        public Provider(string class_name, string formal)
+        {
+            string formal1 = Regex.Replace(formal, @"\s+", string.Empty);
+
+            Regex rxpre = new Regex(@"pre(.*?)post");
+            Regex rxfunc = new Regex(@".*?(?=pre)");
+            Regex rxpost = new Regex(@"post(.+)$");
+
+            function = rxfunc.Match(formal1).Value;
+            pre = rxpre.Match(formal1).Groups[1].Value;
+            post =rxpost.Match(formal1).Groups[1].Value;
+
+            Console.WriteLine("func '{0}'.", function);
+            Console.WriteLine("pre '{0}' .", pre);
+            Console.WriteLine("post '{0}'.", post);
+
+            handleFunction(function);
+            handlePre(pre);
+        }
+
+        void handleFunction(string function)
+        {
+            Regex rx_name = new Regex(@".*(?=\()");
+            Regex rx_variable = new Regex(@"\((.*?)\)");
+            Regex rx_result = new Regex(@"\)(.+)$");
+
+            function_name = rx_name.Match(function).Value;
+            getVariable(rx_variable.Match(function).Groups[1].Value);
+            getResult(rx_result.Match(function).Groups[1].Value);
+
+            Console.WriteLine("func name '{0}'.", function_name);
+            Console.WriteLine("variables '{0}'.", rx_variable.Match(function).Groups[1].Value);
+            Console.WriteLine("result '{0}','{1}'.", function_result.Key,function_result.Value);
+            
+        }
+        void getVariable(string variable)
+        {
+            string[] splitVariable1 = Regex.Split(variable, @",");
+
+            Regex rx_variable_name = new Regex(@".*(?=:)");
+            Regex rx_variable_type = new Regex(@":(.+)$");
+
+            foreach (string value in splitVariable1)
+            {
+                function_variable.Add(rx_variable_name.Match(value).Value, rx_variable_type.Match(value).Groups[1].Value);
+            }
+        }
+        void getResult(string result)
+        {
+            Regex rx_resuilt_name = new Regex(@".*(?=:)");
+            Regex rx_resuilt_type = new Regex(@":(.+)$");
+            function_result = new KeyValuePair<string, string>(rx_resuilt_name.Match(result).Value, rx_resuilt_type.Match(result).Groups[1].Value);
+        }
+
+        void handlePre(string pre)
+        {
+            
+        }
+
+        void handlePost(string post)
+        { 
+        }
+    }
+}
